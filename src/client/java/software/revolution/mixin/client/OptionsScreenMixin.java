e4df28ee96e987b1314af8ebcf1f1f4366a43ddc
Revolution <tavinho.daniel@gmail.com>
2023-08-11 19:59:05 -0300

    feat: add Herobrine Settings screen with localization fix: screen parent bug

--- src/client/java/software/revolution/mixin/client/OptionsScreenMixin.java
+++ src/client/java/software/revolution/mixin/client/OptionsScreenMixin.java
@@ -4,7 +4,9 @@ import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.option.OptionsScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.gui.widget.GridWidget;
+import net.minecraft.client.option.GameOptions;
 import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -16,14 +18,22 @@ import software.revolution.gui.HerobrineSettingsScreen;
 import java.util.function.Supplier;
 
 @Mixin(OptionsScreen.class)
-public abstract class OptionsScreenMixin {
+public abstract class OptionsScreenMixin extends Screen {
+
+    @Shadow
+    @Final
+    private GameOptions settings;
+
+    protected OptionsScreenMixin(Text title) {
+        super(title);
+    }
 
     @Shadow
     protected abstract ButtonWidget createButton(Text text, Supplier<Screen> screenSupplier);
 
     @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/widget/GridWidget$Adder;add(Lnet/minecraft/client/gui/widget/Widget;)Lnet/minecraft/client/gui/widget/Widget;", ordinal = 2), method = "init", locals = LocalCapture.CAPTURE_FAILHARD)
     private void addChild(CallbackInfo ci, GridWidget gridWidget, GridWidget.Adder adder) {
-        adder.add(this.createButton(Text.of("Herobrine Settings"), HerobrineSettingsScreen::new), 2,
+        adder.add(this.createButton(Text.translatable("ui.settings.button"), () -> new HerobrineSettingsScreen(this, this.settings)), 2,
                 adder.copyPositioner().alignRight()
         );
     }
--- src/client/kotlin/software/revolution/gui/HerobrineSettingsScreen.kt
+++ src/client/kotlin/software/revolution/gui/HerobrineSettingsScreen.kt
@@ -2,11 +2,27 @@ package software.revolution.gui
 
 import net.minecraft.client.gui.DrawContext
 import net.minecraft.client.gui.screen.Screen
+import net.minecraft.client.gui.screen.option.GameOptionsScreen
+import net.minecraft.client.gui.widget.ButtonWidget
+import net.minecraft.client.option.GameOptions
 import net.minecraft.text.Text
 
-class HerobrineSettingsScreen : Screen(Text.of("Herobrine Settings")) {
+class HerobrineSettingsScreen(parent: Screen, gameOptions: GameOptions) :
+    GameOptionsScreen(parent, gameOptions, Text.translatable("ui.settings.title")) {
+
+    override fun init() {
+        addDrawableChild(
+            ButtonWidget.builder(Text.translatable("ui.settings.back")) {
+                client?.setScreen(parent)
+            }.build()
+        )
+    }
 
     override fun render(context: DrawContext?, mouseX: Int, mouseY: Int, delta: Float) {
-        context?.fillGradient(0, 0, width, height, -1072689136, -804253680)
+        this.renderBackground(context)
+
+        context?.drawCenteredTextWithShadow(textRenderer, title, width / 2, 15, 16777215)
+
+        super.render(context, mouseX, mouseY, delta)
     }
 }
