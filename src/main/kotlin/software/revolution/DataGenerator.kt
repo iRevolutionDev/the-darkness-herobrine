8d96a662164d92b6bbd4f87d6d9023d257469014
Revolution <tavinho.daniel@gmail.com>
2023-08-11 20:15:21 -0300

    refactor: rename `TheDarknessHerobrineDataGenerator` to `DataGenerator`
    The file and object 'TheDarknessHerob

--- src/main/kotlin/software/revolution/DataGenerator.kt
+++ src/main/kotlin/software/revolution/DataGenerator.kt
@@ -3,7 +3,7 @@ package software.revolution
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
 
-object TheDarknessHerobrineDataGenerator : DataGeneratorEntrypoint {
+object DataGenerator : DataGeneratorEntrypoint {
     override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
 
     }
